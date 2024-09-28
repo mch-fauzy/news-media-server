34cb8205afc7a9b667649c1755798d0481d088e7
mch-fauzy <mucha.fauzy@gmail.com>
2024-09-28 23:39:05 +0700

    feat: add shared folder

---
+++ shared/constants.ts
@@ -0,0 +1,11 @@
+const SHARED = {
+    REDIS: {
+        CACHE_EXPIRY: 60 * 60, // In seconds
+        NEWS_HEADLINES_KEY: 'newsHeadlines'
+    },
+    MESSAGE: {
+        SUCCESS: 'Success'
+    }
+};
+
+export { SHARED }
