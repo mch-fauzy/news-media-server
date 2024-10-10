b51b531a70ca0421ddbabadf79f41fefbc6d7167
mch-fauzy <mucha.fauzy@gmail.com>
2024-10-10 19:09:20 +0700

    feat: add latest build

--- dist/routes/v1/news_headlines.js
+++ dist/routes/v1/news_headlines.js
@@ -8,6 +8,6 @@ exports.newsHeadlinesRouterV1 = newsHeadlinesRouterV1;
 newsHeadlinesRouterV1.get('/news-headlines', news_headlines_1.viewNewsHeadlines);
 newsHeadlinesRouterV1.post('/news-headlines', news_headlines_1.addNewsHeadline);
 newsHeadlinesRouterV1.put('/news-headlines/:id', news_headlines_1.editNewsHeadlineById);
-newsHeadlinesRouterV1.patch('/news-headlines/:id', news_headlines_1.partialEditNewsHeadlineById);
-newsHeadlinesRouterV1.delete('/news-headlines/:id', news_headlines_1.softRemoveNewsHeadlineById);
-newsHeadlinesRouterV1.delete('/news-headlines/hard/:id', news_headlines_1.removeNewsHeadlinesById);
+newsHeadlinesRouterV1.patch('/news-headlines/:id', news_headlines_1.partialEditNewsHeadlineById); // add logic if data is marked as deleted cant be edited and ask db admin to restore 
+newsHeadlinesRouterV1.patch('/news-headlines/:id/deleted', news_headlines_1.softRemoveNewsHeadlineById);
+newsHeadlinesRouterV1.delete('/news-headlines/:id', news_headlines_1.removeNewsHeadlinesById);
