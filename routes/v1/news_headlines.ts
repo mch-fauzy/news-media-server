6a0b6da8b37d149389759fd9d8c5f72207349871
mch-fauzy <mucha.fauzy@gmail.com>
2024-10-10 19:04:55 +0700

    fix: change route name and assign soft remove from delete to patch

--- routes/v1/news_headlines.ts
+++ routes/v1/news_headlines.ts
@@ -6,8 +6,8 @@ const newsHeadlinesRouterV1 = Router();
 newsHeadlinesRouterV1.get('/news-headlines', viewNewsHeadlines);
 newsHeadlinesRouterV1.post('/news-headlines', addNewsHeadline);
 newsHeadlinesRouterV1.put('/news-headlines/:id', editNewsHeadlineById);
-newsHeadlinesRouterV1.patch('/news-headlines/:id', partialEditNewsHeadlineById);
-newsHeadlinesRouterV1.patch('/news-headlines/:id/soft-remove', softRemoveNewsHeadlineById); // better to merge softRemove to partialEdit and add logic if data is marked as deleted cant be edited and ask db admin to restore 
+newsHeadlinesRouterV1.patch('/news-headlines/:id', partialEditNewsHeadlineById); // add logic if data is marked as deleted cant be edited and ask db admin to restore 
+newsHeadlinesRouterV1.patch('/news-headlines/:id/deleted', softRemoveNewsHeadlineById);
 newsHeadlinesRouterV1.delete('/news-headlines/:id', removeNewsHeadlinesById)
 
 export { newsHeadlinesRouterV1 };
